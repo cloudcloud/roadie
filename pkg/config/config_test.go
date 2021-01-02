a7b3fda79cf84bab746f2af4a99fe7e9fd409db5
CerealBoy <allan.shone@gmail.com>
2021-01-02 13:03:48 +1100

    Adding a test to force circle to refresh

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	assert := assert.New(t)
+
+	cases := []struct {
+		Port     string
+		Error    bool
+		Expected *Config
+	}{
+		{
+			Error:    false,
+			Expected: &Config{Hostname: "http://localhost:8080", Listener: ":8008", Location: "/config.roadie.json"},
+			Port:     "",
+		},
+	}
+
+	for _, x := range cases {
+		os.Setenv("PORT", x.Port)
+
+		actual, err := New()
+		assert.Equal(x.Expected, actual)
+		if !x.Error {
+			assert.Nil(err)
+		} else {
+			assert.NotNil(err)
+		}
+	}
+}
